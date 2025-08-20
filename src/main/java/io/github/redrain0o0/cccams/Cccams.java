7d869d591d4aaceecd40af6fde0f07b241eae306
RedRain0o0 <redraino.o.o.o.o@gmail.com>
2025-08-20 12:42:59 -0400

    initial commit

---
+++ src/main/java/io/github/redrain0o0/cccams/Cccams.java
@@ -0,0 +1,10 @@
+package io.github.redrain0o0.cccams;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cccams implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/io/github/redrain0o0/cccams/client/CccamsClient.java
@@ -0,0 +1,10 @@
+package io.github.redrain0o0.cccams.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CccamsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
